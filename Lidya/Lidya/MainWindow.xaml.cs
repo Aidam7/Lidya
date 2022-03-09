4e96db0ff19d20c2c518a13d45b10c23cd1f7be7
Tomáš Diblík <dibla.tomas@post.cz>
2022-03-09 23:15:21 +0100

    [Feature] Read windows into combobox

--- Lidya/Lidya/MainWindow.xaml.cs
+++ Lidya/Lidya/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +25,44 @@ namespace Lidya
         {
             InitializeComponent();
         }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            EnumWindows(new WindowEnumCallback(this.AddWnd), 0);
+            foreach (string? window in Windows) {
+                AvailableProgramsCombo.Items.Add(window);
+            }
+        }
+
+        private void ExecuteActionButton_OnClick(object sender, EventArgs e)
+        {
+            // TODO: Open new window above the desired window
+            string desiredWindow = (string)AvailableProgramsCombo.SelectedItem;
+        }
+
+        /* --- code from https://stackoverflow.com/a/10819641/16638833 --- */
+        public delegate bool WindowEnumCallback(int hwnd, int lparam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumWindows(WindowEnumCallback lpEnumFunc, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern void GetWindowText(int h, StringBuilder s, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(int h);
+
+        private readonly List<string> Windows = new List<string>();
+        private bool AddWnd(int hwnd, int lparam)
+        {
+            if (IsWindowVisible(hwnd)) {
+                StringBuilder sb = new StringBuilder(255);
+                GetWindowText(hwnd, sb, sb.Capacity);
+                Windows.Add(sb.ToString());
+            }
+            return true;
+        }
+        /* --- End of the stolen code --- */
     }
 }
